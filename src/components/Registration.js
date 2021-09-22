5b0eff6320a04b6caa53f1e9d5a8e3759afc8064
mug913 <brad.alexander913@gmail.com>
2021-09-21 19:05:29 -0700

    added registration form component Registration.js

---
+++ src/components/Registration.js
@@ -0,0 +1,31 @@
+import { Component } from 'react'
+
+export default class Registration extends Component {
+
+    state = {
+        username: '',
+        password: '',
+        email: ''
+    }
+
+    // update state for matching input name value
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <form>
+                <label>Username :</label>
+                <input name="username" value={this.state.username} onChange={this.handleChange} />
+                <label>Password :</label>
+                <input type='password' name="password" value={this.state.password} onChange={this.handleChange} />
+                <label>Email :</label>
+                <input type='email' name="email" value={this.state.email} onChange={this.handleChange}  />
+                <input type='Submit' value='Register'/>
+            </form>
+        )
+    }
+}

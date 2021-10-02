be5d7de1cbef6a00783402e9c3220f450dcf8b70
mug913 <brad.alexander913@gmail.com>
2021-10-02 10:43:17 -0700

    added signup component/form for user login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import Registration from './components/Registration';
+import SignIn from './components/SignIn';
 
 class App extends Component {
 
@@ -27,10 +28,31 @@ class App extends Component {
     .then(user => this.setState({ user }) )
   }
 
+  signIn = (user) => {
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user:{
+          email: user.email,
+          password: user.password,
+        }
+      })
+    })
+  }
+
   render(){
     return (
       <div className="App">
-        {this.state.user.username ? <h2>Welcome {this.state.user.username}</h2> : <Registration signUp={this.signUp} />}
+        {this.state.user.username ? <h2>Welcome {this.state.user.username}</h2> : (
+        <>
+        <SignIn signIn={this.signIn} />
+        <Registration signUp={this.signUp} />}
+        </>)
+        }
       </div>
     );
   }
---
+++ src/components/SignIn.js
@@ -0,0 +1,34 @@
+import { Component } from 'react'
+
+export default class SignIn extends Component {
+
+    state = {
+        email: '',
+        password: '',
+    }
+
+    // update state for matching input name value
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.signIn(this.state)
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h1>Log In</h1>
+                <label>Email :</label>
+                <input name="email" value={this.state.username} onChange={this.handleChange} />
+                <label>Password :</label>
+                <input type='password' name="password" value={this.state.password} onChange={this.handleChange} />
+                <input type='Submit' value='Authenticate'/>
+            </form>
+        )
+    }
+}

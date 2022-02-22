f1d32bfbd5ce323d311e323c6ce9f9125c9e35d9
Brad Alexander <brad.alexander913@gmail.com>
2022-02-21 18:09:14 -0800

    began work on DayAreaForm dropdown

--- src/components/DayLog.js
+++ src/components/DayLog.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {DayAreaForm} from './DayAreaForm'
 
 export const DayLog = (props) => {
+const [showPanel, setShowPanel] = useState(false)
 const currentDateButton = new Date().toDateString()
-const dayButton = <button> {currentDateButton} </button>
+const panelContent = <DayAreaForm setShowPanel={setShowPanel} />
+const panel = showPanel ? panelContent : null
 
+const DayButtonClick = (e) =>{
+    e.preventDefault()
+    setShowPanel(!(showPanel))
+}
+
+const dayButton = <button onClick={DayButtonClick}> {currentDateButton} </button>
 
     return(
         <div className='area-0'>
